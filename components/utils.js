53b837702241c03e112d1b1b937db0cd9c2be388
rofviv <royvillarroel94@gmail.com>
2024-06-29 10:42:38 -0400

    new version wp

--- components/utils.js
+++ components/utils.js
@@ -1,22 +1,25 @@
-// https://github.com/wppconnect-team/wa-version/tree/main/html
-const { Client} = require("whatsapp-web.js");
-// const path = require("path");
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const path = require("path");
 
+// const generateClientWhatsapp = () => new Client();
+
+// const generateClientWhatsapp = () =>
+//   new Client({
+//     authStrategy: new LocalAuth({
+//       dataPath: path.join(__dirname, "..", ".wwebjs_auth"),
+//     }),
+//     puppeteer: { headless: true, args: ["--no-sandbox"] },
+//   });
+
+// https://github.com/wppconnect-team/wa-version/tree/main/html
 const generateClientWhatsapp = () =>
   new Client({
-    webVersionCache:
-    {
-      remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.3000.1014576526-alpha.html',
-      type: 'remote'
-    }
+    webVersionCache: {
+      remotePath:
+        "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2413.51-beta.html",
+      type: "remote",
+    },
   });
-// new Client({
-//   authStrategy: new LocalAuth({
-//     dataPath: path.join(__dirname, "..", ".wwebjs_auth"),
-//   }),
-//   puppeteer: { headless: true, args: ["--no-sandbox"] },
-// });
-
 module.exports = {
   generateClientWhatsapp,
 };
