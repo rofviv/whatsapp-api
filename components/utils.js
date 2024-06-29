c51a424dd82531240fd39ffddeda601bd7ed5558
rofviv <royvillarroel94@gmail.com>
2024-06-29 10:06:40 -0400

    version wp

--- components/utils.js
+++ components/utils.js
@@ -1,11 +1,12 @@
-const { Client, LocalAuth } = require("whatsapp-web.js");
-const path = require("path");
+// https://github.com/wppconnect-team/wa-version/tree/main/html
+const { Client} = require("whatsapp-web.js");
+// const path = require("path");
 
 const generateClientWhatsapp = () =>
   new Client({
     webVersionCache:
     {
-      remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+      remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.3000.1014576526-alpha.html',
       type: 'remote'
     }
   });
